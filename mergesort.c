ccccd6479654ec8e6b10bf946895187602f1fa1b
Bryan-linux-repo <beau.dev.boy007@gmail.com>
2023-12-16 14:55:57 -0500

    some files were added over here

---
+++ mergesort.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mergeSort(int arr[], int start, int end);
+void JustMerge(int arr[], int b1, int mid, int b2);
+
+int main(void) {
+
+    int n;
+
+    printf("Please, Type n as the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    
+    printf("Please, Type each of the elements from your Array of numbers below\n");
+    
+    for (int i = 0; i < n; i++) {
+        printf("type %d as: ", i);
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("This Sorted Array :\n\n[ ");
+
+    for (int i = 0; i < n; i++) {
+        printf(" %d ,", arr[i]);
+    }
+    printf("]\n\n");
+
+    return 0;
+}
+
+void mergeSort(int arr[], int start, int end) {
+    int mid;
+
+    if (start < end) {
+        mid = (start + end) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+        JustMerge(arr, start, mid, end);
+    }
+}
+
+void JustMerge(int arr[], int b1, int mid, int b2) {
+    int nleft = (mid - b1 + 1);
+    int nright = (b2 - mid);
+
+    int left_arr[nleft];
+    int right_arr[nright];
+
+    for (int i = 0; i < nleft; i++) {
+        left_arr[i] = arr[b1 + i];
+    }
+
+    for (int j = 0; j < nright; j++) {
+        right_arr[j] = arr[mid + j + 1];
+    }
+
+    int i = 0;
+    int j = 0;
+    int p;
+
+    for (p = b1; p <= b2; p++) {
+        if (i < nleft && (j >= nright || left_arr[i] <= right_arr[j])) {
+            arr[p] = left_arr[i++];
+        } else {
+            arr[p] = right_arr[j++];
+        }
+    }
+}
